2e419609012d812d5d8b867dd7f3e57efa473a83
Jean Plouin <jean@appsoluce.com>
2015-10-14 22:26:35 +0200

    message

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cards');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+	console.log("did connect to cards db");
+});
+
+var cardSchema = mongoose.schema({
+	owner: String,
+	message: String
+});
+
+cardSchema.method.toString = function () {
+	return "card created by " + this.owner + "\n" + "message :" + this.message + "\n\n";
+}
+
+var Card = mongoose.model('Card', cardSchema);
+
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,12 +30,38 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 5000;
 
 app.post('/create_card', function (req, res) {
-	var nickname = req.body.nickname,
+	var owner = req.body.owner,
 		message = req.body.message;
 
-	console.log("got createproject with nickname "+req.body.nickname+" res:"+req.body.message);
-	
-	res.send("YOU DID IT ! your nickname is'"+nickname+"'; the message:'"+message+"'");
+	console.log("got createproject with owner "+owner+" res:"+message);
+
+	var card = new Card({
+		owner: owner,
+		message: message
+	});
+
+	card.save(function (err, card) {
+		if (err) {
+			res.send("ERROR: " + err);
+		} else {
+			res.send("NEW PROJECT CREATED ! owner is'"+owner+"'; message is'"+message+"'");
+		}
+	});
+});
+
+app.post('/get_cards', function (req, res) {
+	var owner = req.body.owner;
+
+	var cards = Card.find({ owner: owner }),
+		responseText = 'PROJECTS BY ' + owner + '\n';
+
+	for (var i = cards.length - 1; i >= 0; i--) {
+		var card = cards[i];
+
+		responseText += card.toString();
+	};
+
+	res.send(responseText);
 });
 
 app.listen(port, function () {});
